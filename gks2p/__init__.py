5f6062bcd918f65c1f1c72cca5909fcdef04f975
Georgios Keliris <keliris@gmail.com>
2024-12-29 18:07:19 -0500

    updated __init__.py

--- gks2p/__init__.py
+++ gks2p/__init__.py
@@ -1,5 +1,5 @@
-from datasets import *
-from preprocess import *
-from mkops import *
-from mkops_general import mkops_general
+from gks2p.datasets import *
+from gks2p.preprocess import *
+from gks2p.mkops import *
+from gks2p.mkops_general import mkops_general
 
