b4bfd6d26d25ce0c19937e037cae1b27046d7fca
Georgios Keliris <keliris@gmail.com>
2025-01-11 18:55:45 -0500

    added log-TM-A

---
+++ scripts/20250109-log-TM-A.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  9 07:17:51 2025
+
+@author: georgioskeliris
+"""
+# 9 Jan 2025
+from gks2p import *
+basepath = '/mnt/NAS_UserStorage/georgioskeliris/MECP2TUN/'
+fastbase = '/mnt/NAS_DataStorage/Data_active/MECP2TUN/'
+ds = datasetQuery(cohort='coh1', week='w22', experiment='DR')
+db = {
+    'pipeline': 'orig',
+    'tau': 1.5,
+    'spatial_scale': 0,
+    'reg_tif': True
+}
+# And make the ops from the .tif header (if not done)
+gks2p_makeOps(ds, basepath, db=db, fastbase=fastbase)
+
+gks2p_toBinary(ds, basepath)
+gks2p_register(ds, basepath, iplaneList=None)
+
+# 10 Jan 2025
+gks2p_segment(ds, basepath, iplaneList=None)
