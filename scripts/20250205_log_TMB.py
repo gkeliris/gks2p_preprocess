c62cef3fab01b86072866eade36188d1f46c8603
Georgios Keliris <keliris@gmail.com>
2025-02-06 10:34:50 -0500

    changed mkops.py to remove negative values from mkops.py

--- scripts/20250205_log_TMB.py
+++ scripts/20250205_log_TMB.py
@@ -6,7 +6,10 @@ Created on Wed Feb  5 15:26:20 2025
 @author: georgioskeliris
 """
 
-from gks2p import *
+from gks2p.datasets import *
+from gks2p.preprocess import *
+from gks2p.mkops import *
+
 basepath = '/mnt/NAS_DataStorage/Data_active/LRN2P/'
 fastbase = '/mnt/NAS_DataStorage/Data_active/LRN2P/'
 
@@ -20,3 +23,8 @@ db = {
 
 
 gks2p_makeOps(ds, basepath, db=db, fastbase=fastbase)
+gks2p_toBinary(ds, basepath)
+gks2p_register(ds, basepath, iplaneList=None)
+gks2p_segment(ds, basepath, iplaneList=None) # potentially can give a list of planes to process
+# Feb 6, 2025
+gks2p_combine(ds, basepath)
