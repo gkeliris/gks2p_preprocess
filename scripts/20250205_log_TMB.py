78a5abe0dcc3da88f734d0cfaaefaa2ec739309b
Georgios Keliris <keliris@gmail.com>
2025-07-08 18:42:08 -0400

    info added to logfile

--- scripts/20250205_log_TMB.py
+++ scripts/20250205_log_TMB.py
@@ -68,3 +68,64 @@ gks2p_toBinary(ds, basepath)
 gks2p_register(ds, basepath, iplaneList=None)
 gks2p_segment(ds, basepath, iplaneList=None) # potentially can give a list of planes to process
 gks2p_combine(ds, basepath)
+
+
+
+
+# 27 Mar 2025
+ds = datasetQuery(cohort='lrn1', day='d6', mouseID='M1')
+db = {
+    'pipeline': 'orig',
+    'tau': 1.5,
+    'spatial_scale': 0,
+    'reg_tif': False
+}
+gks2p_makeOps(ds, basepath, db=db, fastbase=fastbase)
+gks2p_toBinary(ds, basepath)
+gks2p_register(ds, basepath, iplaneList=None)
+
+
+# 28 Mar 2025
+ds = datasetQuery(cohort='lrn1', day='d6', mouseID='M2')
+db = {
+    'pipeline': 'orig',
+    'tau': 1.5,
+    'spatial_scale': 0,
+    'reg_tif': False
+}
+gks2p_makeOps(ds, basepath, db=db, fastbase=fastbase)
+gks2p_toBinary(ds, basepath)
+gks2p_register(ds, basepath, iplaneList=None)
+
+# 29 Mar 2025
+ds = datasetQuery(cohort='lrn1', day='d6', mouseID='M771')
+db = {
+    'pipeline': 'orig',
+    'tau': 1.5,
+    'spatial_scale': 0,
+    'reg_tif': False
+}
+gks2p_makeOps(ds, basepath, db=db, fastbase=fastbase)
+gks2p_toBinary(ds, basepath)
+gks2p_register(ds, basepath, iplaneList=None)
+
+ds = datasetQuery(cohort='lrn1', day='d6', mouseID='M797')
+gks2p_makeOps(ds, basepath, db=db, fastbase=fastbase)
+gks2p_toBinary(ds, basepath)
+gks2p_register(ds, basepath, iplaneList=None)
+
+ds = datasetQuery(cohort='lrn1', day='d6', mouseID='M801')
+gks2p_makeOps(ds, basepath, db=db, fastbase=fastbase)
+gks2p_toBinary(ds, basepath)
+gks2p_register(ds, basepath, iplaneList=None)
+
+
+# 11 Apr 2025
+ds = datasetQuery(cohort='lrn1', day='d0', mouseID='M827', experiment='zstack')
+gks2p_makeOps(ds, basepath, db=db, fastbase=fastbase)
+gks2p_toBinary(ds, basepath)
+gks2p_register(ds, basepath, iplaneList=None)
+
+# 23 Jun 2025
+ds = datasetQuery(cohort='lrn1', day='d0', mouseID='M852', experiment='DR')
+gks2p_combine(ds, basepath)
